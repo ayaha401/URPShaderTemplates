ad10397f91c3c569c746992d0618c2d6e9dacb73
DESKTOP-L97VA5M\oshiz <oshizawadai.1192@gmail.com>
2024-08-18 10:47:30 +0900

    namespaceつけた

--- Scripts/CreateURPShaderTemplates.cs
+++ Scripts/CreateURPShaderTemplates.cs
@@ -1,25 +1,28 @@
 using UnityEditor;
 
-public class CreateURPShaderTemplates : EditorWindow
+namespace AyahaGraphicDevelopTools.URPShaderTemplates
 {
-    private const string UNLIT_TEMPLATE_PATH = "Assets/Editor/AyahaGraphicDevelopTools/URPShaderTemplates/Template/TemplateURPUnlitShader.txt";
-    [MenuItem("Assets/Create/Shader/Unlit Shader(URP)")]
-    private static void CreateURPUnlitTemp()
+    public class CreateURPShaderTemplates : EditorWindow
     {
-        ProjectWindowUtil.CreateScriptAssetFromTemplateFile(UNLIT_TEMPLATE_PATH, "URPUnlit.shader");
-    }
+        private const string UNLIT_TEMPLATE_PATH = "Assets/Editor/AyahaGraphicDevelopTools/URPShaderTemplates/Template/TemplateURPUnlitShader.txt";
+        [MenuItem("Assets/Create/Shader/Unlit Shader(URP)")]
+        private static void CreateURPUnlitTemp()
+        {
+            ProjectWindowUtil.CreateScriptAssetFromTemplateFile(UNLIT_TEMPLATE_PATH, "URPUnlit.shader");
+        }
 
-    private const string UI_DEFAULT_TEMPLATE_PATH = "Assets/Editor/AyahaGraphicDevelopTools/URPShaderTemplates/Template/TemplateURPUIDefaultShader.txt";
-    [MenuItem("Assets/Create/Shader/UI Default Shader(URP)")]
-    private static void CreateURPUITemp()
-    {
-        ProjectWindowUtil.CreateScriptAssetFromTemplateFile(UI_DEFAULT_TEMPLATE_PATH, "URPUI.shader");
-    }
+        private const string UI_DEFAULT_TEMPLATE_PATH = "Assets/Editor/AyahaGraphicDevelopTools/URPShaderTemplates/Template/TemplateURPUIDefaultShader.txt";
+        [MenuItem("Assets/Create/Shader/UI Default Shader(URP)")]
+        private static void CreateURPUITemp()
+        {
+            ProjectWindowUtil.CreateScriptAssetFromTemplateFile(UI_DEFAULT_TEMPLATE_PATH, "URPUI.shader");
+        }
 
-    private const string HLSL_TEMPLATE_PATH = "Assets/Editor/AyahaGraphicDevelopTools/URPShaderTemplates/Template/TemplateHLSL.txt";
-    [MenuItem("Assets/Create/Shader/HLSL Template")]
-    private static void CreateHLSLTemp()
-    {
-        ProjectWindowUtil.CreateScriptAssetFromTemplateFile(HLSL_TEMPLATE_PATH, "Temp.hlsl");
+        private const string HLSL_TEMPLATE_PATH = "Assets/Editor/AyahaGraphicDevelopTools/URPShaderTemplates/Template/TemplateHLSL.txt";
+        [MenuItem("Assets/Create/Shader/HLSL Template")]
+        private static void CreateHLSLTemp()
+        {
+            ProjectWindowUtil.CreateScriptAssetFromTemplateFile(HLSL_TEMPLATE_PATH, "Temp.hlsl");
+        }
     }
 }
